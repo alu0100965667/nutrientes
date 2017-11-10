25a1acae7960493c3019367fb7ba169b69aa22a8
deivbbmd <xdxrovied@gmail.com>
2017-11-10 01:47:10 +0000

    Lista tiene los atributos head y tail

--- lib/alimento/lista.rb
+++ lib/alimento/lista.rb
@@ -1,5 +1,15 @@
 Nodo = Struct.new(:valor, :siguiente, :anterior)
 
 class Lista
+	def initialize(head, tail)
+		@head = head
+		@tail = tail
+	end
 	
+	def head
+		@head
+	end
+	def tail
+		@tail
+	end
 end
