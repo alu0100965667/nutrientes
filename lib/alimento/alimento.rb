3e9a4bacd2f7863803fc3561426dcdcb3701b98c
deivbbmd <xdxrovied@gmail.com>
2017-11-24 11:52:32 +0000

    Documentación escrita y generada

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -32,21 +32,26 @@ class Alimento
 		(@proteinas*4 + @glucidos*4 + @lipidos*9)
 	end
 	
+	# Método auxiliar para hacer el if de la función aibc
 	def if0(gij, g0, gi1)
 		return 0.0 if gij < g0
 		return ((((gij-g0)+(gi1-g0))/2)*5)
 	end
 	
+	# Recibe un vector con los datos de un alimento para los individuos, devuelve un vector con los AIBC de cada individuo
 	def aibc(g)
 		# recibe un vector de datos del alimento
 		# devuelve los aibc del alimento, o glucosa, para cada individuo
 		g.map{ |gi| gi.each_with_index.map{ |gindex, i| if0(gindex, gi[0], gi[i-1]) if i > 0 } }.map{ |gi| gi.reject{ |gij| gij.class == nil::NilClass } }.map{ |ri| ri.reduce(0, :+) }
 	end
 	
+	# Calcula los AIBC de los datos del alimento y los divide entre los AIBC de los datos de la glucosa
+	# Devolviendo un vector con los IG de cada individuo
 	def ig_ind
 		aibc(@datos[0]).each_with_index.map{ |ind, i| (ind/aibc(@datos[1])[i])*100 }
 	end
 	
+	# Calcula el IG del alimento haciendo la media con los IG de los individuos
 	def ig
 		ig_ind.reduce(0, :+)/ig_ind.size
 	end
