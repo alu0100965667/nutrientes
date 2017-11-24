9f2f1c701e17007a19c07910a7c84257d1af640d
deivbbmd <xdxrovied@gmail.com>
2017-11-24 10:45:19 +0000

    Se obtiene el AIBC correctamente

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -32,8 +32,15 @@ class Alimento
 		(@proteinas*4 + @glucidos*4 + @lipidos*9)
 	end
 	
-	def aibc
-		
+	def if0(gij, g0, gi1)
+		return 0.0 if gij < g0
+		return ((((gij-g0)+(gi1-g0))/2)*5)
+	end
+	
+	def aibc(g)
+		# recibe un vector de datos del alimento
+		# devuelve los aibc del alimento, o glucosa, para cada individuo
+		g.map{ |gi| gi.each_with_index.map{ |gindex, i| if0(gindex, gi[0], gi[i-1]) if i > 0 } }.map{ |gi| gi.reject{ |gij| gij.class == nil::NilClass } }.map{ |ri| ri.reduce(0, :+) }
 	end
 	
 end
