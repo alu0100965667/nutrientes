571eb76cf2a743058fe9b3fe0e6b1a38d3231d4e
deivbbmd <xdxrovied@gmail.com>
2017-11-30 19:43:19 +0000

    Prueba de que existe una función que ordene con each, falla

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -292,6 +292,9 @@ RSpec.describe Alimento::GrupoAlimentos do
     it "Ordenando con for" do
       expect(@alimentos.sort_for.to_s).to eq("[Tomate, Calabaza, Cebolla, Manzana, Pera, Leche, Yogurt, Papas, Bacalao, Plátano, Ternera, Pollo, Cerdo, Salmón, Atún, Huevo Frito, Lentejas, Arroz, Azúcar, Chocolate, Mantequilla, Aceite de oliva]")
     end
+    it "Existe una función que ordene con each" do
+      expect(@alimentos).to respond_to(:sort_each)
+    end
   end
   
 end
