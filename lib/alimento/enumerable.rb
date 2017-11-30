7ecf0e998c2e869a65822e1c96bcf3730b78201f
deivbbmd <xdxrovied@gmail.com>
2017-11-30 19:42:00 +0000

    Método sort_for implementado y funciona

--- lib/alimento/enumerable.rb
+++ lib/alimento/enumerable.rb
@@ -1,7 +1,15 @@
 module Enumerable
 	
 	def sort_for
-		
+		doble = self.dup
+		for i in 0...doble.size do
+			for j in 0...doble.size do
+				if doble[i] < doble[j]
+					doble[i], doble[j] = doble[j], doble[i]
+				end
+			end
+		end
+		doble
 	end
 	
 end
